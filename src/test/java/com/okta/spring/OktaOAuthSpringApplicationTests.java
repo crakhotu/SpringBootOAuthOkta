33669a8a477bd0abbc3701cfe1e9298644545af8
Rakesh Siddartha S V <31545942+crakhotu@users.noreply.github.com>
2021-06-29 22:30:11 +0530

    uploading the files for the application

---
+++ src/test/java/com/okta/spring/OktaOAuthSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.okta.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OktaOAuthSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
